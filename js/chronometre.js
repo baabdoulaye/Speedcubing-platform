97185f30c00b530b1b96f7b17c27bc9461ffb750
abdoulaye <abdoulaye_ba@outlook.com>
2023-03-30 16:57:55 +0200

    mise a jour projet 30/03

---
+++ js/chronometre.js
@@ -0,0 +1,7 @@
+//-------------- code que m'a filé alex pour------------------------------------------------
+//  stocker des données
+//   ou dans le cache,
+//   ou dans le navigateur
+// (les données sont direct supprimées quand je ferme ou quand j'actualise le navigateur) en js
+localStorage.setItem();
+// --------------------------------------------------------------------------------------
